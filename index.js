dcd65ec4fdfcc2db11e177c954c3768d0a2b8178
CHOISY Tommy <choisy.tommy@gmail.com>
2016-03-25 23:21:29 +0100

    init plugin

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+module.exports = (pluginContext) => {
+    const shell = pluginContext.shell;
+    const toast = pluginContext.toast;
+
+    function search(query, res) {
+        const query_trim = query.trim();
+        if (query_trim.length == 0) {
+            return;
+        }
+
+        res.add({
+            id: query_trim,
+            payload: 'all',
+            title: 'Search ' + query_trim,
+            desc: 'Search music',
+            icon: "#fa fa-music",
+        });
+        
+    }
+
+    function execute(id, payload) {
+        
+        if (payload == 'all') {
+            shell.openExternal('http://www.allmusic.com/search/all/'+encodeURI(id);
+    
+        }else{
+            return;
+        }
+    }
+
+    return {search, execute};
+};
